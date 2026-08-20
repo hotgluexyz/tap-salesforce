ff567c192b37af6640fef97629a487047cfa375e
Otavio Trentin <67597089+otavio-trentin@users.noreply.github.com>
2026-08-20 12:39:12 -0300

    HGI-10436: bump default api version (#82)
    * bump default api version
    
    * bump version only when necessary
    
    * bump API fallback to V61
    
    * drop unsused fields
    
    * smoke-test to use branch correctly

--- tap_salesforce/salesforce/__init__.py
+++ tap_salesforce/salesforce/__init__.py
@@ -273,7 +273,7 @@ class Salesforce():
         self.rest_requests_attempted = 0
         self.jobs_completed = 0
         self.login_timer = None
-        self.version = api_version or "41.0"
+        self.version = api_version or "61.0"
         self.data_url = "{}/services/data/v" + self.version + "/{}"
         self.pk_chunking = False
 
